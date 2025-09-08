80780fab33e37fccdbea31dae80de4b21c211341
SLPStefan <123269@chrlyceumdelft.nl>
2025-09-08 13:23:46 +0000

    Yallah yallah

--- Opdrachten/PyGame/Les1/Les1.py
+++ Opdrachten/PyGame/Les1/Les1.py
@@ -12,12 +12,32 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((800, 400))
-pygame.display.set_caption('Eerste game!')
+pygame.display.set_caption('Super gevorderde game')
 clock = pygame.time.Clock()
 running = True
 
-surface = pygame.Surface((100, 100))
+surface = pygame.Surface((800, 400))
 surface.fill("blue")
+
+surface2 = pygame.Surface((45, 200))
+surface2.fill("green")
+
+surface3 = pygame.Surface((45, 70))
+surface3.fill("green")
+
+surface4 = pygame.Surface((45, 240))
+surface4.fill("green")
+
+surface5 = pygame.Surface((45, 70))
+surface5.fill("green")
+
+surface6 = pygame.Surface((45, 300))
+surface6.fill("green")
+
+surface7 = pygame.Surface((45, 70))
+surface7.fill("green")
+
+
 
 while running:
 
@@ -26,6 +46,13 @@ while running:
       running = False
 
   screen.blit(surface, (0, 0))
+  screen.blit(surface2, (200, 0))
+  screen.blit(surface3, (400, 0))
+  screen.blit(surface4, (600, 0))
+  screen.blit(surface5, (200, 330))
+  screen.blit(surface6, (400, 170))
+  screen.blit(surface7, (600, 330))
+
   
   pygame.display.update()
   clock.tick(60)
