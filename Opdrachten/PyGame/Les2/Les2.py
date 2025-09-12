ddf63626e50580244e0703b392a81f0d1f2c5498
SLPStefan <123269@chrlyceumdelft.nl>
2025-09-12 09:20:11 +0000

    yallah

--- Opdrachten/PyGame/Les2/Les2.py
+++ Opdrachten/PyGame/Les2/Les2.py
@@ -24,14 +24,44 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 400))
-pygame.display.set_caption('Voetbal game!')
+screen = pygame.display.set_mode((960, 639))
+pygame.display.set_caption('Portugal O - O Argentinië')
 clock = pygame.time.Clock()
 running = True
-test_font = pygame.font.Font("Opdrachten/PyGame/Les2/fonts/horror.ttf", 50)
+test_font = pygame.font.Font("Opdrachten/PyGame/Les2/fonts/Snacker.ttf", 50)
+Lato = pygame.font.Font("Opdrachten/PyGame/Les2/fonts/Lato.ttf", 25)
 
-voetbal_surface = pygame.image.load("Opdrachten/PyGame/Les2/graphics/voetbal.png")
-tekst_surface = test_font.render("Voetbal game", False, "green")
+scoreboards = pygame.Surface((300, 30))
+scoreboards.fill("gray")
+scoreboards2 = pygame.Surface((80, 30))
+scoreboards2.fill("white")
+scoreboards3 = pygame.Surface((70, 30))
+scoreboards3.fill("blue")
+voetbalveld = pygame.image.load("Opdrachten/PyGame/Les2/graphics/veld.jpg")
+voetbal = pygame.image.load("Opdrachten/PyGame/Les2/graphics/voetbal.png")
+scorebord = Lato.render("Por     6-7     No", False, "black")
+minuten = Lato.render("67:16", False, "white")
+naamronaldo = test_font.render("Ronaldo", False, "black")
+naammessi = test_font.render("Messi", False, "black")
+Nor = pygame.image.load("Opdrachten/PyGame/Les2/graphics/Nor.png")
+DEFAULT_IMAGE_SIZE = (Nor.get_width() // 63, Nor.get_height() // 63)
+Nor_small = pygame.transform.scale(Nor, DEFAULT_IMAGE_SIZE)
+
+por = pygame.image.load("Opdrachten/PyGame/Les2/graphics/Portugal.png")
+DEFAULT_IMAGE_SIZE = (por.get_width() // 13, por.get_height() // 13)
+por_small = pygame.transform.scale(por, DEFAULT_IMAGE_SIZE)
+
+
+deronaldo = pygame.image.load("Opdrachten/PyGame/Les2/graphics/deronaldo.png")
+DEFAULT_IMAGE_SIZE = (deronaldo.get_width() // 2, deronaldo.get_height() // 2)
+deronaldo_small = pygame.transform.scale(deronaldo, DEFAULT_IMAGE_SIZE)
+
+messi = pygame.image.load("Opdrachten/PyGame/Les2/graphics/messi.png")
+DEFAULT_IMAGE_SIZE = (messi.get_width() // 1.4, messi.get_height() // 1.4)
+messi_small = pygame.transform.scale(messi, DEFAULT_IMAGE_SIZE)
+
+goalrechts = pygame.image.load("Opdrachten/PyGame/Les2/graphics/goalrechts.png")
+goallinks = pygame.image.load("Opdrachten/PyGame/Les2/graphics/goallinks.png")
 
 while running:
 
@@ -39,8 +69,21 @@ while running:
     if event.type == pygame.QUIT:
       running = False
 
-  screen.blit(tekst_surface, (200, 100))
-  screen.blit(voetbal_surface, (200, 200))
+  screen.blit(voetbalveld,  (0, 0))
+  screen.blit(scoreboards3, (320, 0))
+  screen.blit(scoreboards, (20, 0))
+  screen.blit(scoreboards2, (130, 0))
+  screen.blit(scorebord, (88, 0))
+  screen.blit(minuten, (320, 0))
+  screen.blit(voetbal, (460, 500))
+  screen.blit(Nor_small, (280, 0))
+  screen.blit(por_small, (20, 0))
+  screen.blit(deronaldo_small, (200, 300))
+  screen.blit(messi_small, (400, 300))
+  screen.blit(goalrechts, (850, 200))
+  screen.blit(goallinks, (-200, 200))
+  screen.blit(naamronaldo, (300, 250))
+  screen.blit(naammessi, (540, 250))
   
   pygame.display.update()
   clock.tick(60)
